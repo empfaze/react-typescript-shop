57c725d45b99d416db4be80c43626b897e853ce7
empfaze <empfaze@gmail.com>
2021-12-09 00:14:39 +0300

    Replaced BrowserRouter with HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>,
   document.getElementById("root")
 );
